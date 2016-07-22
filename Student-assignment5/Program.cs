20ce4126532bbea4cda3f3fc84e10634fb9b9024
dhruti <dhruti@DESKTOP-4S1SR41>
2016-07-22 15:26:43 +0100

    student class created

--- Student-assignment5/Program.cs
+++ Student-assignment5/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Student_assignment5
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
+            try
+            {
+                const char DELIM = ',';
+                Console.WriteLine("Enter the name of the File");
+                string FILENAME = Console.ReadLine();
+                if (File.Exists(FILENAME))
+                {
+                    FileStream inFile = new FileStream(FILENAME, FileMode.Open, FileAccess.Read);
+                    StreamReader reader = new StreamReader(inFile);
+                    string recordIn;
+                    string[] fields;
+                    Console.WriteLine("\n{0,-5}{1,-12}{2,8}\n", "Name", "Class", "Grade");
+                    recordIn = reader.ReadLine();
+                    while (recordIn != null)
+                    {
+                        recordIn = reader.ReadLine();
+                        Student std = new Student();
+                        fields = recordIn.Split(DELIM);
+                        std.Name = fields[0];
+                        std.Class = fields[1];
+                        std.Grade = fields[2];
+                        std.ID = fields[3];
+                        Console.WriteLine("{0,5}{1,12}{2,8}{3,10}", std.Name, std.Class, std.Grade, std.ID);
+                        recordIn = reader.ReadLine();
+                    }
+                    reader.Close();
+                    inFile.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
---
+++ Student-assignment5/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_assignment5
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string Grade { get; set; }
+        public string Class { get; set; }
+    }
+}
